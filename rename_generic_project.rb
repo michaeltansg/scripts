dd36dea6ec806884e4f7e2b0a948f299ef1f4ea2
Michael Tan <tan.michael@mac.com>
2020-12-14 15:54:13 +0700

    Create rename_generic_project.rb

---
+++ rename_generic_project.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+require 'find'
+require 'FileUtils'
+
+$generic_project_name = 'FirebaseAppBoilerplate'
+project_name = 'MyApp'
+file_paths = []
+
+def return_first_matching_directory
+  Dir.glob("**/*/").each { |path|
+    return path.delete_suffix('/') if path =~ /#{Regexp.escape($generic_project_name)}/
+  }
+  return nil
+end
+def return_first_matching_file
+  Dir.glob("**/*").each { |path|
+    return path.delete_suffix('/') if path =~ /#{Regexp.escape($generic_project_name)}/
+  }
+  return nil
+end
+
+Find.find('.') do |path|
+  if path =~ /(\.git\/|#{Regexp.escape(File.basename(__FILE__))}$)/
+  else
+    file_paths << path unless File.directory?(path)
+	end
+end
+
+replacements = [
+	[$generic_project_name, project_name]
+]
+
+file_paths.each { |file|
+	lines = File.readlines(file).join('')
+  replacements.each { |replacement| lines.gsub!(replacement[0], replacement[1]) }
+  File.open(file, "w") { |file| file.puts lines }
+}
+
+while dir = return_first_matching_directory do
+  FileUtils.mv dir, dir.gsub($generic_project_name, project_name)
+end
+while file = return_first_matching_file do
+  FileUtils.mv file, file.gsub($generic_project_name, project_name)
+end
